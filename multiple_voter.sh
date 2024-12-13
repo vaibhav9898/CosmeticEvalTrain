f2f9af92ce2f3bc9b67b2b798a92ba88a7d83a20
vaibhav9898 <67003375+vaibhav9898@users.noreply.github.com>
2024-12-13 19:46:23 +0530

    Create multiple_voter.sh

---
+++ multiple_voter.sh
@@ -0,0 +1,9 @@
+python multiple_voter.py \
+    --val_file_path "../Data/test.tsv"\
+    --batch_size 8\
+    --densenet_path "model_checkpoints/current/densenet_Accuracy.pth"\
+    --resnet_path "model_checkpoints/current/resnet_Accuracy.pth"\
+    --mob_path "model_checkpoints/current/mobileVIT_Accuracy.pth"\
+    --swin_path "model_checkpoints/current/swin_Accuracy.pth"
+    
+
